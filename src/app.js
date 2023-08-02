8e05308cd2ea22c0ec5babf9ecf98cfa04a05d7c
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-08-02 13:46:34 -0700

    connected to the port

--- src/app.js
+++ src/app.js
@@ -4,3 +4,11 @@ require("./config/db");
 const express = require("express");
 const bodyParser = express.json;
 const cors = require("cors");
+
+// create server app
+const app = express();
+
+app.use(cors());
+app.use(bodyParser());
+
+module.exports = app;
