2e255b38e32d237391bb66aee8a70affa186b997
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-08-07 14:19:30 -0700

    logistics of signing up

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -13,7 +13,13 @@ router.post("/signup", async(req, res) => {
         if (!(name && email && password)) {
             throw Error("Empty input fields!");
         } else if (!/^[a-zA-Z]*$/.test(name)) {
-            throw Error("Invalid name enrtered");
+            throw Error("Invalid name entered");
+        } else if (!/^[\-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            throw Error("Invalid email entered");
+        } else if (password.legnth < 8) {
+            throw Error("Passw2ord is too short!");
+        } else {
+            
         }
     } catch (error) {
         
