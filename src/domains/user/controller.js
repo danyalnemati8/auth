ecbd54e50850396ec2622a3a1213451cf455687a
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-08-07 14:42:41 -0700

    checking to see if the user already exists

---
+++ src/domains/user/controller.js
@@ -0,0 +1,14 @@
+const User = require("./model");
+
+const creatNewUser =  async (data) => {
+    try {
+        const { name, email, password } = data;
+
+        //checks if the user is already in system
+        const existingUser = await User.findOne({email});
+
+        if (existingUser) {
+            throw Error("User with the provided email already exists");
+        }
+    } catch (error) {}
+};
